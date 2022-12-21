b0b004e52db5dfce9051d902e1e8a02aab86c4f5
JohanLeon0914 <johanalbertols@ufps.edu.co>
2022-12-21 17:10:10 -0500

    deslizar el chat hacia abajo cuando hay nuevos mensajes

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,105 +1,64 @@
-import {
-  Avatar,
-  Button,
-  Flex,
-  FormControl,
-  Heading,
-  Input,
-  Text,
-} from "@chakra-ui/react";
-import Sidebar from "../../components/Sidebar";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { useCollectionData, useDocumentData } from "react-firebase-hooks/firestore";
-import { addDoc, collection, doc, orderBy, query, serverTimestamp } from "firebase/firestore";
-import { auth, db } from "../../firebaseconfig";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { Flex, Text } from "@chakra-ui/layout"
+import Sidebar from "../../components/Sidebar"
+import Head from "next/head"
+import { useRouter } from "next/router"
+import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { collection, doc, orderBy, query } from "firebase/firestore"
+import { db, auth } from "../../firebaseconfig"
+import Topbar from "../../components/Topbar"
+import Bottombar from "../../components/Bottombar"
+import { useRef, useEffect } from "react";
 import getOtherEmail from "../../util/getOtherEmail";
-import { useState } from "react";
-
-
-const Topbar = ({email}) => {
-  return (
-    <Flex bg="gray.100" h="81px" w="100%" align="center" p={5}>
-      <Avatar src="" marginEnd={3} />
-      <Heading size="lg"> {email} </Heading>
-    </Flex>
-  );
-};
-
-const BottomBar = ({id, user}) => {
-
-  const [input, setInput] = useState("")
-
-  const sendMessage = async (e) => {
-    e.preventDefault()
-    await addDoc(collection(db, `/chats/${id}/messages`), {
-        text: input,
-        sender: user.email,
-        timestamp: serverTimestamp()
-    });
-    setInput("");
-  }
-
-  return (
-    <FormControl p={3}
-        onSubmit={sendMessage}
-        as='form'
-    >
-      <Input placeholder="Escribe un mensaje..." autoComplete="off" onChange={e => setInput(e.target.value)} value={input} />
-      <Button type="submit" hidden onClick={e => sendMessage(e)} >
-        Submit
-      </Button>
-    </FormControl>
-  );
-};
 
 export default function Chat() {
   const router = useRouter();
   const { id } = router.query;
-
+  const [user] = useAuthState(auth);
+  const [chat] = useDocumentData(doc(db, "chats", id));
   const q = query(collection(db, `chats/${id}/messages`), orderBy("timestamp"));
   const [messages] = useCollectionData(q);
-  const [user] = useAuthState(auth); 
-  const [chat] = useDocumentData(doc(db, "chats", id))
-  
+  const bottomOfChat = useRef();
 
   const getMessages = () =>
     messages?.map(msg => {
-        const sender = msg.sender === user.email
+      const sender = msg.sender === user.email;
       return (
-        <Flex key={Math.random()} alignSelf={sender? "flex-start": "flex-end"} bg={ sender? "blue.100": "green.100"} w="fit-content" minWidth="100px" borderRadius="lg" p={3} m={1}>
+        <Flex key={Math.random()} alignSelf={sender ? "flex-start" : "flex-end"} bg={sender ? "blue.100" : "green.100"} w="fit-content" minWidth="100px" borderRadius="lg" p={3} m={1}>
           <Text>{msg.text}</Text>
         </Flex>
       )
     })
 
+  useEffect(() => {
+    setTimeout(
+      bottomOfChat.current.scrollIntoView({
+      behavior: "smooth",
+      block: 'start',
+    }), 100)
+  }
+  , [messages])
+
   return (
-    <Flex h="100vh">
-      <Head>
-        <title>Grupo de chat</title>
-      </Head>
+    <Flex
+      h="100vh"
+    >
+      <Head><title>Chat App</title></Head>
 
       <Sidebar />
 
       <Flex flex={1} direction="column">
         <Topbar email={getOtherEmail(chat?.users, user)} />
 
-        <Flex
-          flex={1}
-          direction="column"
-          pt={4}
-          mx={5}
-          overflowX="scroll"
-          sx={{ scrollbarWidth: "none" }}
-          className="chats-container"
-        >
+        <Flex flex={1} direction="column" pt={4} mx={5} overflowX="scroll" sx={{scrollbarWidth: "none"}} className='chats-container'>
           {getMessages()}
-          
+          <div ref={bottomOfChat}></div>
         </Flex>
 
-        <BottomBar id={id} user={user} />
+        <Bottombar id={id} user={user} />
       </Flex>
+
     </Flex>
-  );
+
+  )
 }
